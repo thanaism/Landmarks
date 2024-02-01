236cde4d0cc3f684d285337200de1d73c5eb3728
thanaism <62784012+thanaism@users.noreply.github.com>
2024-02-02 01:11:43 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hifumi Takai on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
