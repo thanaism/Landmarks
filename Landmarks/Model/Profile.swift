c44eaea57bfaa9dbb0ed4598c2910dee434faed1
thanaism <62784012+thanaism@users.noreply.github.com>
2024-02-05 00:57:24 +0900

    Working with UI controlsのチュートリアルを完了

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Hifumi Takai on 2024/02/05.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
